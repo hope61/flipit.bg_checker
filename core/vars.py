da2460dd217140cd67252e6c93d0e7d49734c8b4
Hope <hope@h0pe.tk>
2023-05-29 17:27:51 +0300

    added more info in vars.py

--- core/vars.py
+++ core/vars.py
@@ -2,9 +2,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-url = 'https://flip.bg/magazin/apple/mobilen-telefon-apple-iphone-xs-256gb-space-grey/158/?%D1%81%D1%8A%D1%81%D1%82%D0%BE%D1%8F%D0%BD%D0%B8%D0%B5=Dobro'
+url = '' # the url to the phone u want to keep a watch on
 web_hook = os.getenv('web_hook')
-min_price = 800.00
+min_price = 6969.00 #the min price of the phone this is the price that triggers the bot to send a message
 
+# DONT TOUCH
 total_requests = 0
 below_min_price_count = 0
